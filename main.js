698a038c65cd3da963c54ff5fb173d4225c05a2d
Jose Akle <deltaalpha94@hotmail.com>
2022-11-05 23:14:53 -0500

    Add Fisher Yates Shuffle function

--- main.js
+++ main.js
@@ -44,3 +44,13 @@ const randomOptions = [
     'finance status'
   ],
 ];
+
+const fYatesShuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let k = arr[i];
+    arr[i] = arr[j];
+    arr[j] = k;
+  }
+  return arr[0];
+}
